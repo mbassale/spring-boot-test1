f7f9a0a1e008a83553bbc45e7e555b90f601cec5
Marco Bassaletti <bassaletti@gmail.com>
2021-04-05 23:29:08 -0400

    Update and delete persons.

--- src/main/java/cl/mbassale/test1/dao/FakePersonDataAccessService.java
+++ src/main/java/cl/mbassale/test1/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -21,5 +22,32 @@ public class FakePersonDataAccessService implements PersonDao {
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        var personMaybe = selectPersonById(id);
+        if (personMaybe.isPresent()) {
+            DB.remove(personMaybe.get());
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return selectPersonById(id).map(p -> {
+            int indexPersonToUpdate = DB.indexOf(p);
+            if (indexPersonToUpdate >= 0) {
+                DB.set(indexPersonToUpdate, new Person(id, person.getName()));
+                return 1;
+            }
+            return 0;
+        }).orElse(0);
     }
 }
